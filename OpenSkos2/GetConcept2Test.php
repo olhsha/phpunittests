049393ebb0236dd50046477e35a5b32edf23dcae
olha <olha@olhas-mbp.ia.knaw.local>
2015-10-27 19:38:53 +0100

    Get test is extended. solr is not injected, had to patch openskos2, and it looks like i have to bypass solr layer at all.

--- OpenSkos2/GetConcept2Test.php
+++ OpenSkos2/GetConcept2Test.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__DIR__) . '/Utils/Authenticator.php'; 
+//require_once dirname(__DIR__) . '/Utils/Authenticator.php'; 
 require_once dirname(__DIR__) . '/Utils/RequestResponse.php'; 
 
 class GetConcept2Test extends PHPUnit_Framework_TestCase {
@@ -8,11 +8,13 @@ class GetConcept2Test extends PHPUnit_Framework_TestCase {
     protected $client;
     protected $response0; 
     protected $prefLabel;
+    protected $altLabel;
+    protected $hiddenLabel;
     protected $notation;
     
     protected function setUp() {
-        $this->client = Authenticator::authenticate();
-
+        //$this->client = Authenticator::authenticate();
+        $this->client = new Zend_Http_Client();
         $this->client->SetHeaders(array(
             'Accept' => 'text/html,application/xhtml+xml,application/xml',
             'Content-Type' => 'text/xml',
@@ -23,11 +25,15 @@ class GetConcept2Test extends PHPUnit_Framework_TestCase {
         // create a test concept
         $randomn = rand(0, 2048);
         $this->prefLabel = 'testPrefLable_' . $randomn;
+        $this->altLabel = 'testAltLable_' . $randomn;
+        $this->hiddenLabel = 'testHiddenLable_' . $randomn;
         $this->notation = 'test-xxx-' . $randomn;
         $xml = '<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:skos="http://www.w3.org/2004/02/skos/core#" xmlns:openskos="http://openskos.org/xmlns#" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmi="http://dublincore.org/documents/dcmi-terms/#">' .
                 '<rdf:Description rdf:about="http://192.168.99.100/api/collections/mi:collection/' . $this->notation . '">' .
                 '<rdf:type rdf:resource="http://www.w3.org/2004/02/skos/core#Concept"/>' .
                 '<skos:prefLabel xml:lang="nl">' . $this->prefLabel . '</skos:prefLabel>' .
+                '<skos:altLabel xml:lang="nl">' . $this->altLabel . '</skos:altLabel>' .
+                '<skos:hiddenLabel xml:lang="nl">' . $this->hiddenLabel . '</skos:hiddenLabel>' .
                 '<openskos:set rdf:resource="' . BASE_URI_ . CONCEPT_collection . '"/>' .
                 '<skos:notation xml:lang="nl">' . $this->notation . '</skos:notation>' .
                 '<skos:inScheme  rdf:resource="http://data.beeldengeluid.nl/gtaa/Onderwerpen"/>' .
@@ -38,9 +44,11 @@ class GetConcept2Test extends PHPUnit_Framework_TestCase {
 
 
         $this->response0 = RequestResponse::CreateConceptRequest($this->client, $xml, "false");
+        print "\n Creation status: " . $this->response0 -> getStatus();
+        
     }
    
-    /*
+    
     public function testViaPrefLabel2() {
         print "\n" . "Test: get concept-rdf via its prefLabel. ";
         
@@ -52,7 +60,7 @@ class GetConcept2Test extends PHPUnit_Framework_TestCase {
                 print "\n " . $response->getMessage();
             }
             $this->AssertEquals(200, $response->getStatus());
-            $this ->assertionsForXMLRDFConcept($response, $this -> prefLabel, "nl", "testje (voor def ingevoegd)",  $this -> notation, 1, 1);
+            $this ->assertionsForXMLRDFConcept($response, $this -> prefLabel, $this -> altLabel, $this -> hiddenLabel, "nl", "testje (voor def ingevoegd)",  $this -> notation, 1, 1);
             
         } else {
             print "\n Cannot perform the test because something is wrong with creating a test concept: " . $this -> response0->getHeader('X-Error-Msg');
@@ -61,7 +69,7 @@ class GetConcept2Test extends PHPUnit_Framework_TestCase {
     
    
     public function testViaPrefLabelImplicit2() {
-        print "\n" . "Test: get concept-rdf via its prefLabel without saying that this is a prefLabel ";
+        print "\n" . "Test: get concept-rdf via its prefLabel, without syaing that this is a pref label";
         
         
         if ($this -> response0->isSuccessful()) {
@@ -72,14 +80,55 @@ class GetConcept2Test extends PHPUnit_Framework_TestCase {
                 print "\n " . $response->getMessage();
             }
             $this->AssertEquals(200, $response->getStatus());
-            $this ->assertionsForXMLRDFConcept($response, $this -> prefLabel, "nl", "testje (voor def ingevoegd)",  $this -> notation, 1, 1);
+            $this ->assertionsForXMLRDFConcept($response, $this -> prefLabel, $this -> altLabel, $this -> hiddenLabel, "nl", "testje (voor def ingevoegd)",  $this -> notation, 1, 1);
             
         } else {
             print "\n Cannot perform the test because something is wrong with creating a test concept: " . $this -> response0->getHeader('X-Error-Msg');
         }
     }
-   */
-     
+    
+    
+    
+    public function testViaAltLabelImplicit2() {
+        print "\n" . "Test: get concept-rdf via its altLabel";
+        
+        
+        if ($this -> response0->isSuccessful()) {
+            // we can now perform the get-test
+            $this -> client->setUri(BASE_URI_ . '/public/api/find-concepts?q=' . $this -> altLabel);
+            $response = $this -> client->request(Zend_Http_Client::GET);
+            if ($response->getStatus() != 200) {
+                print "\n " . $response->getMessage();
+            }
+            $this->AssertEquals(200, $response->getStatus());
+            $this ->assertionsForXMLRDFConcept($response, $this -> prefLabel, $this -> altLabel, $this -> hiddenLabel,"nl", "testje (voor def ingevoegd)",  $this -> notation, 1, 1);
+            
+        } else {
+            print "\n Cannot perform the test because something is wrong with creating a test concept: " . $this -> response0->getHeader('X-Error-Msg');
+        }
+    }
+    
+    
+    public function testViaHiddenLabelImplicit2() {
+        print "\n" . "Test: get concept-rdf via its hiddenLabel";
+        
+        
+        if ($this -> response0->isSuccessful()) {
+            // we can now perform the get-test
+            $this -> client->setUri(BASE_URI_ . '/public/api/find-concepts?q=' . $this -> hiddenLabel);
+            $response = $this -> client->request(Zend_Http_Client::GET);
+            if ($response->getStatus() != 200) {
+                print "\n " . $response->getMessage();
+            }
+            $this->AssertEquals(200, $response->getStatus());
+            $this ->assertionsForXMLRDFConcept($response, $this -> prefLabel, $this -> altLabel, $this -> hiddenLabel,"nl", "testje (voor def ingevoegd)",  $this -> notation, 1, 1);
+            
+        } else {
+            print "\n Cannot perform the test because something is wrong with creating a test concept: " . $this -> response0->getHeader('X-Error-Msg');
+        }
+    }
+    
+    
     public function testViaPrefLabelIncomplete() {
         print "\n" . "Test: get concept-rdf via its prefLabel's prefix ";
         
@@ -99,27 +148,192 @@ class GetConcept2Test extends PHPUnit_Framework_TestCase {
         }
     }
     
+    
+    public function testViaPrefLabelIncompleteAndOneRow() {
+        print "\n" . "Test: get concept-rdf via its prefLabel's prefix, but asking for 1 row ";
+        
+        
+        if ($this -> response0->isSuccessful()) {
+            // we can now perform the get-test
+            $this -> client->setUri(BASE_URI_ . '/public/api/find-concepts?q=prefLabel:testPrefLable*&rows=1');
+            $response = $this -> client->request(Zend_Http_Client::GET);
+            if ($response->getStatus() != 200) {
+                print "\n " . $response->getMessage();
+            }
+            $this->AssertEquals(200, $response->getStatus());
+            $this -> assertionsForManyConceptsRows($response, 1);
+            
+        } else {
+            print "\n Cannot perform the test because something is wrong with creating a test concept: " . $this -> response0->getHeader('X-Error-Msg');
+        }
+    }
+    
+     public function testViaPrefLabelIncompleteAndTwoRows() {
+        print "\n" . "Test: get concept-rdf via its prefLabel's prefix, but asking for 2 rows ";
+        
+        
+        if ($this -> response0->isSuccessful()) {
+            // we can now perform the get-test
+            $this -> client->setUri(BASE_URI_ . '/public/api/find-concepts?q=prefLabel:testPrefLable*&rows=2');
+            $response = $this -> client->request(Zend_Http_Client::GET);
+            if ($response->getStatus() != 200) {
+                print "\n " . $response->getMessage();
+            }
+            $this->AssertEquals(200, $response->getStatus());
+            $this -> assertionsForManyConceptsRows($response, 2);
+            
+        } else {
+            print "\n Cannot perform the test because something is wrong with creating a test concept: " . $this -> response0->getHeader('X-Error-Msg');
+        }
+    }
+    
+    
+    
+    public function testViaPrefLabelAndLangExist2() {
+        print "\n" . "Test: get concept-rdf via its prefLabel and language. ";
+        
+        if ($this -> response0->isSuccessful()) {
+            // we can now perform the get-test
+            $this -> client->setUri(BASE_URI_ . '/public/api/find-concepts?q=prefLabel@nl:' . $this -> prefLabel);
+            $response = $this -> client->request(Zend_Http_Client::GET);
+            if ($response->getStatus() != 200) {
+                print "\n " . $response->getMessage();
+            }
+            $this->AssertEquals(200, $response->getStatus());
+            $this ->assertionsForXMLRDFConcept($response, $this -> prefLabel, $this -> altLabel, $this -> hiddenLabel, "nl", "testje (voor def ingevoegd)",  $this -> notation, 1, 1);
+            
+        } else {
+            print "\n Cannot perform the test because something is wrong with creating a test concept: " . $this -> response0->getHeader('X-Error-Msg');
+        }
+    }
+    
+    
+    public function testViaPrefLabelAndLangDoesNotExist2() {
+        print "\n" . "Test: get concept-rdf via its prefLabel and laguage. Empty result. ";
+        
+        if ($this -> response0->isSuccessful()) {
+            // we can now perform the get-test
+            $this -> client->setUri(BASE_URI_ . '/public/api/find-concepts?q=prefLabel@en:' . $this -> prefLabel);
+            $response = $this -> client->request(Zend_Http_Client::GET);
+            if ($response->getStatus() != 200) {
+                print "\n " . $response->getMessage();
+            }
+            $this->AssertEquals(200, $response->getStatus());
+            $this ->assertionsForManyConceptsZeroResults($response);
+            
+        } else {
+            print "\n Cannot perform the test because something is wrong with creating a test concept: " . $this -> response0->getHeader('X-Error-Msg');
+        }
+    }
+    
+    
+    public function testViaPrefLabelPrefixAndLangExist2() {
+        print "\n" . "Test: get concept-rdf via its prefLabel and language. ";
+        
+        if ($this -> response0 -> getStatus() == 201) {
+            // we can now perform the get-test
+            $this -> client->setUri(BASE_URI_ . '/public/api/find-concepts?q=prefLabel@nl:testPref*');
+            $response = $this -> client->request(Zend_Http_Client::GET);
+            if ($response->getStatus() != 200) {
+                print "\n " . $response->getMessage();
+            }
+            $this->AssertEquals(200, $response->getStatus());
+            $this ->assertionsForManyConcepts($response);
+            
+        } else {
+            print "\n Cannot perform the test because something is wrong with creating a test concept: " . $this -> response0->getBody();
+        }
+    }
+   
+    
+    public function testViaIdXML() {
+        print "\n" . "Test: get concept-rdf via its id. ";
+        
+        if ($this -> response0->isSuccessful()) {
+            $uuid = $this -> getUuidFromResponse($this -> response0);
+            // we can now perform the get-test
+            // since create generates no uuid, this test still fails.
+            $this -> client->setUri(BASE_URI_ . '/public/api/concept/' . $uuid);
+            $response = $this -> client->request(Zend_Http_Client::GET);
+            if ($response->getStatus() != 200) {
+                print "\n " . $response->getMessage();
+            }
+            $this->AssertEquals(200, $response->getStatus());
+            $this ->assertionsForXMLRDFConcept($response, $this -> prefLabel, $this -> altLabel, $this -> hiddenLabel, "nl", "testje (voor def ingevoegd)",  $this -> notation, 1, 1);
+            
+        } else {
+            print "\n Cannot perform the test because something is wrong with creating a test concept: " . $this -> response0->getHeader('X-Error-Msg');
+        }
+    }
+   
+    private function getUuidFromResponse($response){
+        
+        $dom = new Zend_Dom_Query();
+        $namespaces = RequestResponse::setNamespaces();
+        $dom->registerXpathNamespaces($namespaces);
+        $xml = $response->getBody();
+        $dom->setDocumentXML($xml);
+        
+        $sanityCheck = $dom->queryXpath('/rdf:RDF');
+        $this -> AssertEquals(1, count($sanityCheck));
+        $results1 = $dom->queryXpath('/rdf:RDF/rdf:Description');
+        $this -> AssertEquals(1, count($results1));
+        $results2 = $dom->queryXpath('/rdf:RDF/rdf:Description/openskos:uuid');
+        $this -> AssertEquals(1, count($results2));
+        return $results2 -> current() -> nodeValue;
+        
+    }
+    
+    private function assertionsForManyConceptsRows($response, $rows) {
+        
+        $dom = new Zend_Dom_Query();
+        $namespaces = RequestResponse::setNamespaces();
+        $dom->registerXpathNamespaces($namespaces);
+        $xml = $response->getBody();
+       $dom->setDocumentXML($xml);
+        
+        $sanityCheck = $dom->queryXpath('/rdf:RDF');
+        $this -> AssertEquals(1, count($sanityCheck));
+        $results2 = $dom->queryXpath('/rdf:RDF/rdf:Description');
+        $this -> AssertEquals($rows, count($results2));
+    }
+    
+    private function assertionsForManyConceptsZeroResults($response) {
+        
+        $dom = new Zend_Dom_Query();
+        $namespaces = RequestResponse::setNamespaces();
+        $dom->registerXpathNamespaces($namespaces);
+        $xml = $response->getBody();
+        $dom->setDocumentXML($xml);
+        
+        $sanityCheck = $dom->queryXpath('/rdf:RDF');
+        $this -> AssertEquals(1, count($sanityCheck));
+        $results1 = $dom->queryXpath('/rdf:RDF') -> current() -> getAttribute('openskos:numFound');
+        $results2 = $dom->queryXpath('/rdf:RDF/rdf:Description');
+        $this -> AssertEquals(0, count($results2));
+        $this -> AssertEquals(0, intval($results1));
+    }
+    
     private function assertionsForManyConcepts($response) {
         
         $dom = new Zend_Dom_Query();
         $namespaces = RequestResponse::setNamespaces();
         $dom->registerXpathNamespaces($namespaces);
         $xml = $response->getBody();
-        
-        //corretions should be removed after bug fixing
-        $xmlCorrected = str_replace ('xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"', 'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"', $xml);
-        $xmlCorrected1 = str_replace ('dc:creator', 'dcterms:creator', $xmlCorrected);
+          //corretions should be removed after bug fixing
+        $xmlCorrected1 = str_replace ('dc:creator', 'dcterms:creator', $xml);
         $dom->setDocumentXML($xmlCorrected1);
         
         $sanityCheck = $dom->queryXpath('/rdf:RDF');
         $this -> AssertEquals(1, count($sanityCheck));
         $results1 = $dom->queryXpath('/rdf:RDF') -> current() -> getAttribute('openskos:numFound');
         $results2 = $dom->queryXpath('/rdf:RDF/rdf:Description');
+        print "\n numFound =" . intval($results1) . "\n";
         $this -> AssertEquals(intval($results1), count($results2));
         
     }
     
-    private function assertionsForXMLRDFConcept($response, $prefLabel,  $lang, $definition, $notation, $topConceptOf, $inScheme) {
+    private function assertionsForXMLRDFConcept($response, $prefLabel,  $altLabel, $hiddenLabel, $lang, $definition, $notation, $topConceptOf, $inScheme) {
         
         $dom = new Zend_Dom_Query();
         $namespaces = RequestResponse::setNamespaces();
@@ -127,9 +341,7 @@ class GetConcept2Test extends PHPUnit_Framework_TestCase {
         $xml = $response->getBody();
         
         //corretions should be removed after bug fixing
-        $xmlCorrected = str_replace ('xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"', 'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"', $xml);
-        $xmlCorrected1 = str_replace ('dc:creator', 'dcterms:creator', $xmlCorrected);
-        var_dump($xmlCorrected1);
+        $xmlCorrected1 = str_replace ('dc:creator', 'dcterms:creator', $xml);
         $dom->setDocumentXML($xmlCorrected1);
         
         $results1 = $dom->queryXpath('/rdf:RDF/rdf:Description');
@@ -151,6 +363,14 @@ class GetConcept2Test extends PHPUnit_Framework_TestCase {
         $results6 = $dom->query('skos:prefLabel');
         $this -> AssertEquals($lang, $results6 -> current()-> getAttribute('xml:lang'));
         $this -> AssertEquals($prefLabel, $results6 -> current()-> nodeValue);
+        
+        $results6a = $dom->query('skos:altLabel');
+        $this -> AssertEquals($lang, $results6a -> current()-> getAttribute('xml:lang'));
+        $this -> AssertEquals($altLabel, $results6a -> current()-> nodeValue);
+        
+        $results6b = $dom->query('skos:hiddenLabel');
+        $this -> AssertEquals($lang, $results6b -> current()-> getAttribute('xml:lang'));
+        $this -> AssertEquals($hiddenLabel, $results6b -> current()-> nodeValue);
         
         $results7 = $dom->query('skos:definition');
         $this -> AssertEquals($definition, $results7 -> current()-> nodeValue);
