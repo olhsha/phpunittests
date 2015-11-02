fc8c1d5d05d976ebb9ae9d82f0054a0eb76d3a76
olha <olha@192.168.99.1>
2015-11-02 18:12:48 +0100

    Delete test refactored and extended so that the deleted concepts are checked via GET (in the case of deletion 404 must be returned by GET). Also, Added check on "delete approved" which fails since approved concept cannot be simply deleted, but its status must be changed to deprecated.

--- OpenSkos2/DeleteConcept2Test.php
+++ OpenSkos2/DeleteConcept2Test.php
@@ -6,15 +6,15 @@ require_once dirname(__DIR__) . '/Utils/Logging.php';
 class DeleteConcept2Test extends PHPUnit_Framework_TestCase {
   
     private static $client;
-    private static $prefLabel;
-    private static $altLabel;
-    private static $hiddenLabel;
-    private static $uuid;
     private static $about;
-    private static $notation;
+    private static $uuid;
     private static $response0;
     private static $aboutC;
+    private static $uuidC;
     private static $response0C;
+    private static $aboutApproved;
+    private static $response0Approved;
+    private static $uuidApproved;
     
     public static function setUpBeforeClass() {
 
@@ -28,22 +28,18 @@ class DeleteConcept2Test extends PHPUnit_Framework_TestCase {
         );
         // create a test concept
         $randomn = rand(0, 2048);
-        self::$prefLabel = 'testPrefLable_' . $randomn;
-        self::$altLabel = 'testAltLable_' . $randomn;
-        self::$hiddenLabel = 'testHiddenLable_' . $randomn;
-        self::$notation = 'test-xxx-' . $randomn;
+        $prefLabel = 'testPrefLable_' . $randomn;
+        $notation = 'test-xxx-' . $randomn;
         self::$uuid = uniqid();
-        self::$about = BASE_URI_ . CONCEPT_collection . "/" . self::$notation;
+        self::$about = BASE_URI_ . CONCEPT_collection . "/" . $notation;
         $xml = '<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:skos="http://www.w3.org/2004/02/skos/core#" xmlns:openskos="http://openskos.org/xmlns#" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmi="http://dublincore.org/documents/dcmi-terms/#">' .
                 '<rdf:Description rdf:about="' . self::$about . '">' .
                 '<rdf:type rdf:resource="http://www.w3.org/2004/02/skos/core#Concept"/>' .
-                '<skos:prefLabel xml:lang="nl">' . self::$prefLabel . '</skos:prefLabel>' .
-                '<skos:altLabel xml:lang="nl">' . self::$altLabel . '</skos:altLabel>' .
-                '<skos:hiddenLabel xml:lang="nl">' . self::$hiddenLabel . '</skos:hiddenLabel>' .
+                '<skos:prefLabel xml:lang="nl">' . $prefLabel . '</skos:prefLabel>' .
                 '<openskos:set rdf:resource="' . BASE_URI_ . CONCEPT_collection . '"/>' .
                 '<openskos:uuid>' . self::$uuid . '</openskos:uuid>' .
                 '<openskos:tenant>' . COLLECTION_1_tenant . '</openskos:tenant>' .
-                '<skos:notation xml:lang="nl">' . self::$notation . '</skos:notation>' .
+                '<skos:notation xml:lang="nl">' . $notation . '</skos:notation>' .
                 '<skos:inScheme  rdf:resource="http://data.beeldengeluid.nl/gtaa/Onderwerpen"/>' .
                 '<skos:topConceptOf rdf:resource="http://data.beeldengeluid.nl/gtaa/Onderwerpen"/>' .
                 '<skos:definition xml:lang="nl">testje (voor def ingevoegd)</skos:definition>' .
@@ -57,19 +53,15 @@ class DeleteConcept2Test extends PHPUnit_Framework_TestCase {
         
         $randomnC = rand(0, 2048);
         $prefLabelC = 'testPrefLable_' . $randomnC;
-        $altLabelC = 'testAltLable_' . $randomnc;
-        $hiddenLabelC = 'testHiddenLable_' . $randomn;
         $notationC = 'test-xxx-' . $randomnC;
-        $uuidC = uniqid();
+        self::$uuidC = uniqid();
         self::$aboutC = BASE_URI_ . CONCEPT_collection . "/" . $notationC;
         $xmlC = '<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:skos="http://www.w3.org/2004/02/skos/core#" xmlns:openskos="http://openskos.org/xmlns#" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmi="http://dublincore.org/documents/dcmi-terms/#">' .
                 '<rdf:Description rdf:about="' . self::$aboutC . '">' .
                 '<rdf:type rdf:resource="http://www.w3.org/2004/02/skos/core#Concept"/>' .
                 '<skos:prefLabel xml:lang="nl">' . $prefLabelC . '</skos:prefLabel>' .
-                '<skos:altLabel xml:lang="nl">' . $altLabelC . '</skos:altLabel>' .
-                '<skos:hiddenLabel xml:lang="nl">' . $hiddenLabelC . '</skos:hiddenLabel>' .
                 '<openskos:set rdf:resource="' . BASE_URI_ . CONCEPT_collection . '"/>' .
-                '<openskos:uuid>' . $uuidC . '</openskos:uuid>' .
+                '<openskos:uuid>' . self::$uuidC . '</openskos:uuid>' .
                 '<openskos:status>candidate</openskos:status>' .
                 '<openskos:tenant>' . COLLECTION_1_tenant . '</openskos:tenant>' .
                 '<skos:notation xml:lang="nl">' . $notationC . '</skos:notation>' .
@@ -82,6 +74,20 @@ class DeleteConcept2Test extends PHPUnit_Framework_TestCase {
         self::$response0C = RequestResponse::CreateConceptRequest(self::$client, $xmlC, "false");
         //var_dump(self::$response0C -> getBody());
         print "\n Create status: " . self::$response0C -> getStatus() . "\n";
+        
+        $randomnApproved = rand(0, 2048);
+        $prefLabelApproved = 'testPrefLable_' . $randomnApproved;
+        $notationApproved = 'test-xxx-' . $randomnApproved;
+        self::$uuidApproved = uniqid();
+        self::$aboutApproved = BASE_URI_ . CONCEPT_collection . "/" . $notationApproved;
+        $xmlApproved = str_replace($prefLabelC, $prefLabelApproved, $xmlC);
+        $xmlApproved = str_replace(self::$aboutC, self::$aboutApproved, $xmlApproved);
+        $xmlApproved = str_replace($notationC, $notationApproved, $xmlApproved);
+        $xmlApproved = str_replace(self::$uuidC, self::$uuidApproved, $xmlApproved);
+        self::$response0Approved = RequestResponse::CreateConceptRequest(self::$client, $xmlApproved, "false");
+        //var_dump(self::$response0C -> getBody());
+        print "\n Create status: " . self::$response0Approved -> getStatus() . "\n";
+        
     }
     
     public function testDelete() {
@@ -91,6 +97,12 @@ class DeleteConcept2Test extends PHPUnit_Framework_TestCase {
                 Logging::failureMessaging($response, "delete concept");
             }
             $this->AssertEquals(200, $response->getStatus());
+            self::$client->setUri(BASE_URI_ . '/public/api/concept/'. self::$uuid);
+            $responseCheck = self::$client->request('GET');
+            if ($responseCheck -> getStatus() != 404) {
+                Logging::failureMessaging(self::$response0, "delete concept");
+            }
+            
         } else {
             Logging::failureMessaging(self::$response0, "create test concept");
         }
@@ -103,14 +115,29 @@ class DeleteConcept2Test extends PHPUnit_Framework_TestCase {
                 Logging::failureMessaging($response, "delete candidate concept");
             }
             $this->AssertEquals(200, $response->getStatus());
+            self::$client->setUri(BASE_URI_ . '/public/api/concept/'. self::$uuidC);
+            $responseCheck = self::$client->request('GET');
+            if ($responseCheck -> getStatus() != 404) {
+                Logging::failureMessaging(self::$response0, "delete candidate concept");
+            }
+        } else {
+            Logging::failureMessaging(self::$response0, "create test candidate concept");
+        }
+    }
+    
+    public function testDeleteApproved() {
+        if (self::$response0C->getStatus() == 201) {
+            $response = RequestResponse::DeleteRequest(self::$client, self::$aboutApproved);
+            self::$client->setUri(BASE_URI_ . '/public/api/concept/'. self::$uuidApproved);
+            $responseCheck = self::$client->request('GET');
+            if ($responseCheck -> getStatus() == 404) {
+                print "\n Approved concept is not found after deletion attempt! \n";
+            }
+            $this->AssertNotEquals(200, $response->getStatus());
+            
         } else {
             Logging::failureMessaging(self::$response0, "create test candidate concept");
         }
     }
 
-    private function failureMessaging($response, $action) {
-        print "\n Failed to " . $action. ", response header: " . $response->getHeader('X-Error-Msg');
-        print "\n Failed to " . $action. ", response message: " . $response->getMessage();
-        print "\n Failed to " . $action. ", responce message: " . $response->getBody();
-    }
 }
